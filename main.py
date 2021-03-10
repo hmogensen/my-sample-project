7f0f2449642818552ecb1c82c9adc1b08202d451
Hannes Mogensen <hmogensen@users.noreply.github.com>
2021-03-10 16:05:06 +0100

    Add dummy funciton

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello")
+# Return 0 if system is OK, otherwise a positive integer corresponding to an error code
+def check_if_system_is_ok():
+  return 0
+
+return_value = check_if_system_is_ok()
+
+if return_value != 0:
+  raise Exception("System returned error code " + str(return_value))
